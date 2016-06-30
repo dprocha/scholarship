2f9f00b774c60b9da6eb59537d8f56be74843b27
dprocha <ioe3074!>
2016-06-30 17:48:17 -0300

    Foi

--- analytics/urls.py
+++ analytics/urls.py
@@ -3,6 +3,5 @@ from . import views
 
 app_name = 'analytics'
 urlpatterns = [
-    url(r'^$', views.IndexView.as_view(), name='index'),
-    url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
+    url(r'^$', views.index, name='index'),
 ]
--- analytics/views.py
+++ analytics/views.py
@@ -1,19 +1,66 @@
-from django.shortcuts import get_object_or_404, render
-from django.http import HttpResponseRedirect
-from django.core.urlresolvers import reverse
+from django.shortcuts import render
 from django.views import generic
+from django.db.models import Count
 
 from .models import ScholarshipInfo
 
+def index(request):
+    universidade_list = ScholarshipInfo.objects \
+        .values('nm_instituicao_ensino_superior') \
+        .annotate(dcount=Count('nm_instituicao_ensino_superior')) \
+        .order_by('nm_instituicao_ensino_superior')
 
-class IndexView(generic.ListView):
-    template_name = 'pages/index.html'
-    context_object_name = 'scholarship_list'
+    programa_list = ScholarshipInfo.objects \
+        .values('nm_programa') \
+        .annotate(dcount=Count('nm_programa')) \
+        .order_by('nm_programa')
 
-    def get_queryset(self):
-        return ScholarshipInfo.objects.order_by('vl_ano')
+    ano_list = ScholarshipInfo.objects \
+        .values('vl_ano') \
+        .annotate(dcount=Count('vl_ano')) \
+        .order_by('vl_ano')
 
+    kwargs = {
+    }
 
-class DetailView(generic.DetailView):
-    model = ScholarshipInfo
-    template_name = 'pages/detail.html'
+    universidade = request.POST.get('universidade')
+    if universidade == "" or universidade is None:
+       universidade = None
+    else:
+        kwargs['nm_instituicao_ensino_superior'] = universidade
+
+    ano = request.POST.get('ano')
+    if ano == "" or ano is None:
+       ano = None
+    else:
+        kwargs['vl_ano'] = ano
+
+    programa = request.POST.get('programa')
+    if programa == "" or programa is None:
+        programa = None
+    else:
+        kwargs['nm_programa'] = programa
+
+    bolsa_estudo_list = list()
+
+    context = {'universidade_list': universidade_list, \
+               'ano_list': ano_list, \
+               'programa_list': programa_list, \
+               'universidade': universidade, \
+               'ano': ano, \
+               'programa': programa}
+
+    if universidade is not None or \
+            ano is not None or \
+            programa is not None:
+
+        bolsa_estudo_list = ScholarshipInfo.objects \
+            .filter(**kwargs) \
+            .order_by('vl_ano', 'nm_instituicao_ensino_superior', 'nm_programa', 'nm_programa_fomento', 'nm_area_conhecimento')
+    else:
+        pesquisar = request.POST.get('pesquisar')
+        if pesquisar == "":
+            context['error_message'] = "Selecione pelo menos um filtro para realizar pesquisa"
+
+    context['bolsa_estudo_list'] = bolsa_estudo_list
+    return render(request, 'pages/index.html', context)
